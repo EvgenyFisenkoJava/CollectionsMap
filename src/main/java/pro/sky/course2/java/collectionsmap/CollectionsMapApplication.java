cceb7a1379a426b2f66fca2fa777453dfb54e1cb
Evgeny Fisenko <evgeny.f.aircelle@gmail.com>
2022-06-30 22:59:46 +1000

    Initial commit

---
+++ src/main/java/pro/sky/course2/java/collectionsmap/CollectionsMapApplication.java
@@ -0,0 +1,13 @@
+package pro.sky.course2.java.collectionsmap;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CollectionsMapApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CollectionsMapApplication.class, args);
+    }
+
+}
---
+++ src/test/java/pro/sky/course2/java/collectionsmap/CollectionsMapApplicationTests.java
@@ -0,0 +1,13 @@
+package pro.sky.course2.java.collectionsmap;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CollectionsMapApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
